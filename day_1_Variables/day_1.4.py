0bbeacff34bfb291e9acc354712930541807a705
Yusuf-YF <yusuf.yf88@gmail.com>
2021-06-30 17:28:41 +0300

    add day_1

---
+++ day_1_Variables/day_1.4.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+
+# add third vairable c and switch it's value with a, a with b, and b with c.
+c = a
+a = b
+b = c
+
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
