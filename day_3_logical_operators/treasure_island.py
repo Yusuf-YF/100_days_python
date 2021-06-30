c1d16183c2fe358ce03f75959ff0461ae8f3c14d
Yusuf-YF <yusuf.yf88@gmail.com>
2021-06-30 17:30:01 +0300

    add day_3

---
+++ day_3_logical_operators/treasure_island.py
@@ -0,0 +1,43 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+choice = input("You're in the game now, where you wanna go next? left or right? ")
+
+if choice == 'left':
+    choice2 = input("You come across a river, what you wanna do? wait or swim? ")
+    if choice2 == 'wait':
+        choice3 = input("You come across three doors, what door you want to open red, yellow and blue? ")
+        if choice3 == 'yellow':
+            print("Congratulations! you found the treasure.")
+        elif choice3 == 'red':
+            print("You Burned by fire, Game Over")
+        else:
+            print("You have eaten by beasts, Game Over")
+    else:
+        print("You're attacked by trout, Game Over")
+else:
+    print("You fall into a hole, Game Over.")
