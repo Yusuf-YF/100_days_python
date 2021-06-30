bd2f435297769c4dc99c117afe625592f22f6846
Yusuf-YF <yusuf.yf88@gmail.com>
2021-06-30 18:17:37 +0300

    Update password_manager.py

--- day_30_errors_exceptions_json_data/password_manager.py
+++ day_30_errors_exceptions_json_data/password_manager.py
@@ -46,30 +46,27 @@ def save():
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Ops!", message="Please make sure that you haven't left any fields empty.")
-    is_ok = messagebox.askokcancel(title="Confirmation", message=f"You have entered:\nWebsite: {website}\n"
-                                                                 f"Email/Username: {email}\n"
-                                                                 f"Password: {password}\n"
-                                                                 f"Are you sure you want to save?")
-    if is_ok:
-        try:
-            with open("data.json", mode="r") as data_file:
-                # READING OLD DATA.
-                data = json.load(data_file)
-        except FileNotFoundError:
-            with open("data.json", mode="w") as data_file:
-                json.dump(new_data, data_file, indent=4)
-        else:
-            # UPDATING OLD DATA WITH NEW DATA.
-            data.update(new_data)
-
-            # SAVING UPDATED DATA.
-            with open("data.json", mode="w") as data_file:
-                json.dump(data, data_file, indent=4)
-        finally:
-            messagebox.showinfo(title="Saved Password", message="Your password information have been saved!")
-            website_entry.delete(0, END)
-            email_entry.delete(0, END)
-            password_entry.delete(0, END)
+    else:
+        is_ok = messagebox.askokcancel(title="Confirmation", message=f"You have entered:\nWebsite: {website}\n"
+                                                                     f"Email/Username: {email}\n"
+                                                                     f"Password: {password}\n"
+                                                                     f"Are you sure you want to save?")
+        if is_ok:
+            try:
+                with open("data.json", mode="r") as data_file:
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", mode="w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data.json", mode="w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
+                messagebox.showinfo(title="Saved Password", message="Your password information have been saved!")
+                website_entry.delete(0, END)
+                email_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # --------------------- FIND PASSWORD --------------------- #
@@ -86,7 +83,7 @@ def find_password():
             password = data[website]["password"]
             messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
         else:
-            messagebox.showinfo(title="Error", message=f"No Details for {website} Exists.")
+            messagebox.showinfo(title="Error", message="No Details Exists.")
 
 
 # --------------------- UI SETUP --------------------- #
@@ -106,7 +103,7 @@ email_label = Label(text="Email/Username:", bg=BG_COLOR)
 email_label.grid(column=0, row=2)
 password_label = Label(text="Password:", bg=BG_COLOR)
 password_label.grid(column=0, row=3)
-copy_mark = Label(fg=GREEN, bg=BG_COLOR, font=(FONT_NAME, 20, "bold"))
+copy_mark = Label(fg=GREEN, bg=BG_COLOR, font=(FONT_NAME, 16, "bold"))
 copy_mark.grid(column=1, row=5)
 
 website_entry = Entry(width=32)
@@ -125,4 +122,3 @@ add_button = Button(text="Add", width=35, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
-
