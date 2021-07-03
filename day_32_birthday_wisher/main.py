30a62faf7af009f6bbaaef885ada5624b23b37d9
Yusuf-YF <yusuf.yf88@gmail.com>
2021-07-03 16:58:47 +0300

    add day_32

---
+++ day_32_birthday_wisher/main.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+EMAIL = "***"
+PASSWORD = "***"
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthday_dict:
+    person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL, to_addrs=person["email"], msg=f"Subject:Happy Birthday!\n\n{contents}")
